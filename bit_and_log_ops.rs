314ae4865fe92595572bc1a32f3f37044b5638e1
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-02-12 21:36:38 +0530

    bitwise and logical, along with asser! only (_eq!)

---
+++ bit_and_log_ops.rs
@@ -0,0 +1,11 @@
+//logical and bitwise ops
+
+fn main(){
+    
+    assert!(true || false);
+    //assert_eq!(true, false); this will panic and will gen a warning
+    assert!(!true == false);
+    println!("Success");
+    
+    //XOR op --> if same (1,1) then 0 and if diff(0,1) then 1
+}
